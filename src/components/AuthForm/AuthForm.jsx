89eaa58e8e963333855ad7bfd9172fbcdc4dbed9
Stas-0k <brickbybrick15@gmail.com>
2023-05-19 17:30:49 +0300

    commit
    commit

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,6 +1,21 @@
 import css from '../AuthForm/AuthForm.module.css';
+import { NavLink } from 'react-router-dom';
+import { BsEyeSlash, BsEye } from 'react-icons/bs';
+import { useState } from 'react';
 
 const AuthForm = () => {
+  
+  const [isActive, setActive] = useState(false);
+  const handleClick = () => {
+    setActive(!isActive);
+  };
+  const [isActive2, setActive2] = useState(false);
+  const handleClick2 = () => {
+    setActive2(!isActive2);
+  }
+
+
+  
   return (
     <div className={css.container}>
       <h2 className={css.header}>Registration</h2>
@@ -14,28 +29,45 @@ const AuthForm = () => {
             className={css.inputRegister}
           ></input>
         </label>
-        <label>
-          <input
-            placeholder="Password"
-            type="password"
-            name="password"
-            className={css.inputRegister}
-          ></input>
-        </label>
-        <label>
-          <input
-            placeholder="Confirm password"
-            type="password"
-            name="confirm password"
-            className={css.inputRegister}
-          ></input>
-        </label>
-        <button type="submit" className={css.bttnRegister}>Registration</button>
-          </form>
-          <div className={css.containerLog}>
-      <p className={css.text}>Already have an account?</p>
-              <a className={css.link} href="/">Login</a>
-              </div>
+        <div className={css.passwordCont}>
+          <label>
+            <input
+              placeholder="Password"
+              type="password"
+              name="password"
+              className={css.inputRegister}
+            ></input>
+            <button type="button" className={css.eyeBttn} onClick={handleClick}>
+              {isActive ? <BsEye size={24} color="rgba(84, 173, 255, 1)" />
+              : <BsEyeSlash size={24} color="rgba(84, 173, 255, 1)"/>}
+              
+            </button>
+          </label>
+        </div>
+        <div className={css.passwordCont}>
+          <label>
+            <input
+              placeholder="Confirm password"
+              type="password"
+              name="confirm password"
+              className={css.inputRegister}
+            ></input>
+            <button type="button" className={css.eyeBttn} onClick={handleClick2}>
+              {isActive2 ? <BsEye size={24} color="rgba(84, 173, 255, 1)" />
+              : <BsEyeSlash size={24} color="rgba(84, 173, 255, 1)"/>}
+            </button>
+          </label>
+        </div>
+        <button type="submit" className={css.bttnRegister}>
+          Registration
+        </button>
+      </form>
+      <div className={css.containerLog}>
+        <p className={css.text}>Already have an account?</p>&nbsp;
+        <NavLink className={css.link} to="/login">
+          Login
+        </NavLink>
+      </div>
     </div>
   );
 };
