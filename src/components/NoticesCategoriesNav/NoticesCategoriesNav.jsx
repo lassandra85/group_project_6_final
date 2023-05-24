9c1575ace086aff1ec74f4702f64695edc9cf56d
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-05-24 15:01:12 +0300

    add category jsx

--- src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
+++ src/components/NoticesCategoriesNav/NoticesCategoriesNav.jsx
@@ -1,6 +1,25 @@
-const NoticesCategoriesNav = () => {
+import filters from './filters'
+
+
+const NoticesCategoriesNav = ({isUser}) => {
+    const items = filters.map(({filter,path},index)=>{
+        if(!isUser&&filter ==='favorite ads'){
+            return null
+        }
+        if(!isUser&&filter==='my ads'){
+            return null;
+        }
+
+        return (
+            <li key={index}>
+                <button to={`/notices/${path}`}>{filter}</button>
+            </li>
+        )
+
+    })
+    
     return (
-        <></>
+        <ul>{items}</ul>
     );
 }
 
