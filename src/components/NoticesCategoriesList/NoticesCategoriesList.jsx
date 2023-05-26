b6f7c3324da4af9054f7403d8fb93ba7a57aacfb
VladimirPavlov1 <pavvvlovvvvvvladimir@gmail.com>
2023-05-26 22:38:54 +0300

    _node

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -1,6 +1,21 @@
+import { AiOutlineHeart } from "react-icons/ai";
+import { FiTrash2 } from "react-icons/fi";
 const NoticesCategoriesList = () => {
     return (
-        <></>
+        <ul className="List">
+            <li className="ListItem">
+                <div className="ImageWrapper">
+                    <p className="Category"></p>
+                    <button className="FavoriteBtn">
+                        <AiOutlineHeart/> {/*HeartIcon*/}
+                        {<button className="DeleteBtn">
+                            <FiTrash2/>
+                        </button>}
+                    </button>
+                </div>
+
+            </li>
+        </ul>
     );
 }
 
