f50e720774ee7856a318631ec49aff2a42a7542a
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-05-29 23:17:35 +0300

    update pets

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -4,7 +4,6 @@ import LogOut from 'components/LogOut/LogOut';
 import { getUserInfo } from 'redux/auth/operations';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import PetsData from 'components/UserPageComponents/PetsData/PetsData';
 // import { userStateValues } from 'redux/user/selectors';
 
 const UserPage = () => {
