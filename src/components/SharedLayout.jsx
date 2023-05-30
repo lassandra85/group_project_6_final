e16c8fefef48eefa7fa12ff68704a28414f6f9ca
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2023-05-30 23:12:13 +0200

    Burger

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,25 +1,20 @@
 import Header from './Header/Header';
-import Section from './Section/Section';
-import Container from './Container/Container';
 import Footer from './Footer/Footer';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Loader from './Loader/loader';
+// import Loader from './Loader/loader';
 import css from './SharedLayout.module.css';
 
 const SharedLayout = () => {
   return (
     <div className={css.contentWrapper}>
       <Header />
-      <Suspense fallback={<Loader />}>
-        <Outlet />
-      </Suspense>
       <main>
-        <Section>
-          <Container>Hi there!!!</Container>
-        </Section>
         <Toaster position="top-right" reverseOrder={false} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
       <Footer />
     </div>
