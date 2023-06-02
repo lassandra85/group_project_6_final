ba928e981e3e82fcbd5946ce8656bda4d2a4e404
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-06-02 13:50:13 +0300

    added refreshing user

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -5,15 +5,13 @@ export const selectAuth = state => {
 
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
-export const selectUser = state => state.auth.user;
+export const selectIsRefreshing = state => state.auth.isRefreshing;
 
+export const selectUser = state => state.auth.user;
 
 export const selectPets = state => state.auth.pets;
 
 export const selectNewUser = state => state.auth.user.isNewUser;
-
-
-
 
 export const selectIsLoading = state => state.auth.isLoading;
 
