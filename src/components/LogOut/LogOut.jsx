f7b19d557eabb4ab9f4f6eb531a5d1eb25f1457f
lassandra85 <lassandra85@gmail.com>
2023-06-02 13:40:04 +0200

    LogOut

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -1,18 +1,67 @@
+import { useState, useEffect } from 'react';
 import { CiLogout } from 'react-icons/ci';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import ModalLogOut from '../ModalLogOut/ModalLogOut';
+import Modal from '../Modal/Modal';
 import { logOut } from 'redux/auth/operations';
-import styles from './LogOut.module.css'
+import css from './LogOut.module.css'
+import notify from '../../helpers/notification';
+
 
 const LogOut = () => {
-  const dispatch = useDispatch();
 
-    return (
-      <div className={styles.logout}>
-        <CiLogout size={24} fill="#54ADFF" />
-        <button type="button" className={styles.button} onClick={()=> dispatch(logOut()) }>
-          Log Out
-        </button>
-      </div>
+  const [isLogOut, setIsLogOut] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(logOut());
+  }, [dispatch]);
+
+  const handleLogOut = () => {
+    setIsLogOut(true);  
+  };
+
+  const toggleModal = () => {
+    setIsLogOut(false);
+  };
+
+  const handleLogOutYes = async () => {
+    
+    try {
+      dispatch(logOut());
+      setIsLogOut(false);
+      navigate('/');
+      notify('info', 'You have logged out');
+    } catch (error) {
+      notify('error', "Please try again");
+    }
+
+  };
+
+  return (
+       <>
+          <div className={css.logout}>
+        
+            <CiLogout size={24} fill="#54ADFF" />
+            <button type="button" className={css.button} onClick = {handleLogOut} >
+              
+              Log Out
+            
+            </button>
+        
+          </div>
+      
+          {isLogOut && (
+        
+            <Modal toggleModal={toggleModal}>
+          
+              <ModalLogOut toggleModal={toggleModal} handleLogOut = {handleLogOutYes} />
+            
+            </Modal>
+          )}
+        </>
     );
 }
 
